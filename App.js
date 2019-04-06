067ceb1081fc986e7ff831d1ff56e7b4dd752d4d
Mateusz Nowak <mateusz.nowak@swmansion.com>
2019-04-06 18:16:28 +0200

    Send POST with photo to the server and parse response

--- App.js
+++ App.js
@@ -12,6 +12,13 @@ import { RNCamera } from 'react-native-camera';
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor() {
+    super();
+
+    this.state = {
+      rec: 'blebleble 1.0000',
+    };
+  }
   render() {
     return (
       <View style={styles.container}>
@@ -21,7 +28,7 @@ export default class App extends Component<Props> {
           }}
           style={styles.preview}
           type={RNCamera.Constants.Type.back}
-          flashMode={RNCamera.Constants.FlashMode.on}
+          flashMode={RNCamera.Constants.FlashMode.off}
           permissionDialogTitle={'Permission to use camera'}
           permissionDialogMessage={'We need your permission to use your camera phone'}
           onGoogleVisionBarcodesDetected={({ barcodes }) => {
@@ -33,15 +40,55 @@ export default class App extends Component<Props> {
             <Text style={{ fontSize: 14 }}> SNAP </Text>
           </TouchableOpacity>
         </View>
+        {this.state.rec && (
+          <View style={styles.label}>
+            <Text style={styles.text}>{this.state.rec}</Text>
+          </View>
+        )}
       </View>
     );
   }
 
   takePicture = async function() {
     if (this.camera) {
-      const options = { quality: 0.5, base64: true };
+      const options = { quality: 0.5, base64: false };
       const data = await this.camera.takePictureAsync(options);
-      console.log(data.uri);
+      console.log(data);
+
+      var formData = new FormData();
+      formData.append('file', {
+        uri: data.uri,
+        name: 'image.jpg',
+        type: 'multipart/form-data',
+      });
+
+      console.log(formData);
+
+      const root = this;
+
+      fetch('http://ml.piotrknapczyk.com', {
+        method: 'POST',
+        body: formData,
+      })
+        .then(response => {
+          var respText = response.text().then(function(text) {
+            console.log(text);
+            var options = text.slice(1, text.length - 1).split(',');
+
+            var object = options.map(function(obj) {
+              var lines = obj.split('\n');
+              return (
+                lines[1]
+                  .split(':')[1]
+                  .trim()
+                  .replace(/"/g, '') + lines[2].split(':')[1]
+              );
+            })[0];
+            root.setState({ rec: object });
+          });
+        })
+        .then(success => console.log(success))
+        .catch(error => console.log(error));
     }
   };
 }
@@ -65,5 +112,19 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     alignSelf: 'center',
     margin: 20,
+  },
+  label: {
+    position: 'absolute',
+    top: 50,
+    width: '100%',
+    alignItems: 'center',
+  },
+  text: {
+    textAlign: 'center',
+    padding: 20,
+    backgroundColor: 'rgba(52, 52, 52, 0.65)',
+    borderRadius: 15,
+    color: 'white',
+    fontSize: 20,
   },
 });
